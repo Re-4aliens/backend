a7f7c70b3af080e5a100a9232977aed68355a167
김명준 <86913355+mjj111@users.noreply.github.com>
2024-01-12 23:22:22 +0900

    test(AuthDocTest) : 로그인 기능을 API 문서화 하도록 테스트코드 작성

---
+++ src/test/java/com/aliens/backend/docs/AuthDocTest.java
@@ -0,0 +1,52 @@
+package com.aliens.backend.docs;
+
+import com.aliens.backend.auth.controller.dto.AuthToken;
+import com.aliens.backend.auth.controller.dto.LoginRequest;
+import com.aliens.backend.auth.service.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@SpringBootTest
+class AuthDocTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthService authService;
+    @Test
+    void create() throws Exception {
+        final LoginRequest request = new LoginRequest("email","password");
+        final AuthToken response = new AuthToken("accessToken", "refreshToken");
+        when(authService.login(any())).thenReturn(response);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        this.mockMvc.perform(post("/authentication")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("auth-login",
+                        requestFields(
+                                fieldWithPath("email").description("이메일"),
+                                fieldWithPath("password").description("비밀번호")
+                        )
+                ));
+    }
+}

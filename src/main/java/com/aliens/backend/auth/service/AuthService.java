a575bd40874db6f0abeed1d36c378c730ed8f399
김명준 <86913355+mjj111@users.noreply.github.com>
2024-01-16 19:02:08 +0900

    feat(AuthService) : 테스트 성공을 위한 임시구현

--- src/main/java/com/aliens/backend/auth/service/AuthService.java
+++ src/main/java/com/aliens/backend/auth/service/AuthService.java
@@ -6,7 +6,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+    public static final String LOGOUT_SUCCESS = "로그아웃 되었습니다.";
+
     public AuthToken login(LoginRequest loginRequest) {
+        return new AuthToken("accessToken", "refreshToken");
+    }
+
+    public String logout(final AuthToken authToken) {
+        return LOGOUT_SUCCESS;
+    }
+
+    public AuthToken reissue(final AuthToken authToken) {
         return new AuthToken("accessToken", "refreshToken");
     }
 }

821a103ad69308cf0f04769671420adccd92ff26
김명준 <86913355+mjj111@users.noreply.github.com>
2024-01-16 19:18:08 +0900

    test(AuthServiceTest) : 로그인, 로그아웃, 토큰 재발급에 대한 테스트코드 작성

---
+++ src/test/java/com/aliens/backend/auth/service/AuthServiceTest.java
@@ -0,0 +1,62 @@
+package com.aliens.backend.auth.service;
+
+import com.aliens.backend.JWTProperties;
+import com.aliens.backend.auth.controller.dto.AuthToken;
+import com.aliens.backend.auth.controller.dto.LoginRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceTest {
+
+    @Autowired
+    AuthService authService;
+    @Autowired
+    JWTProperties jwtProperties;
+
+    @Test
+    @DisplayName("로그인 성공")
+    void loginTest() {
+        //Given
+        LoginRequest loginRequest = new LoginRequest("tmp@example.com", "tmpPassword");
+
+        //When
+        AuthToken response = authService.login(loginRequest);
+
+        //Then
+        Assertions.assertNotNull(response);
+    }
+
+    @Test
+    @DisplayName("로그아웃 성공")
+    void logoutTest() {
+        //Given
+        LoginRequest loginRequest = new LoginRequest("tmp@example.com", "tmpPassword");
+        AuthToken authToken = authService.login(loginRequest);
+
+        //When
+        String response = authService.logout(authToken);
+
+        //Then
+        Assertions.assertEquals(response, AuthService.LOGOUT_SUCCESS);
+    }
+
+    @Test
+    @DisplayName("토큰 재발급 성공")
+    void reissueTest() {
+        //Given
+        LoginRequest loginRequest = new LoginRequest("tmp@example.com", "tmpPassword");
+        jwtProperties.setAccessTokenValidTime(1L); //AccessToken 유효기한 짧게변경
+        AuthToken requestAuthToken = authService.login(loginRequest);
+        jwtProperties.setAccessTokenValidTime(86400000L); //AccessToken 유효기한 원상복구
+
+        //When
+        AuthToken responseAuthToken = authService.reissue(requestAuthToken);
+
+        //Then
+        Assertions.assertNotEquals(requestAuthToken, responseAuthToken);
+    }
+}

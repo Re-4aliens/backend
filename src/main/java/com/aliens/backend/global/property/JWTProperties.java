eb856ffd4a46eb3f83d95324c2131d8ba51f1a93
김명준 <86913355+mjj111@users.noreply.github.com>
2024-01-17 00:15:26 +0900

    feat(JWTProperties) : 변수 및 get 메서드 구현

--- src/main/java/com/aliens/backend/global/property/JWTProperties.java
+++ src/main/java/com/aliens/backend/global/property/JWTProperties.java
@@ -1,9 +1,39 @@
 package com.aliens.backend.global.property;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JWTProperties {
-    public void setAccessTokenValidTime(final long i) {
+    @Value("${jwt.secret_key}")
+    private String secretKey;
+
+    @Value("${jwt.access_token_valid_time}")
+    private long accessTokenValidTime;
+
+    @Value("${jwt.refresh_token_valid_time}")
+    private long refreshTokenValidTime ;
+
+    @Value("${jwt.chat_token_valid_time}")
+    private long chatTokenValidTime ;
+
+
+    public byte[] getBytesSecretKey() {
+        return secretKey.getBytes(StandardCharsets.UTF_8);
+    }
+
+    public long getRefreshTokenValidTime() {
+        return refreshTokenValidTime;
+    }
+
+    public long getAccessTokenValidTime() {
+        return accessTokenValidTime;
+    }
+
+    public void setAccessTokenValidTime(Long time) {
+        accessTokenValidTime = time;
     }
 }
+

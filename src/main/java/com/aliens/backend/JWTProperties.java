8d0879a46adb17bcb693d36e7fb251e24b8e32dc
김명준 <86913355+mjj111@users.noreply.github.com>
2024-01-16 19:19:06 +0900

    feat(JWTProperties) : 토큰재발급 테스트코드 성공을 위한 클래스 생성

---
+++ src/main/java/com/aliens/backend/JWTProperties.java
@@ -0,0 +1,9 @@
+package com.aliens.backend;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTProperties {
+    public void setAccessTokenValidTime(final long i) {
+    }
+}

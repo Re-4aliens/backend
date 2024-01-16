cca9525d5b2b507fb6501a7558785cc4e222ba55
김명준 <86913355+mjj111@users.noreply.github.com>
2024-01-16 19:00:39 +0900

    test(AuthDocTest) : 로그인, 로그아웃, 토큰 재발급에 대한 controller 테스트 코드 추가

--- src/test/java/com/aliens/backend/docs/AuthDocTest.java
+++ src/test/java/com/aliens/backend/docs/AuthDocTest.java
@@ -17,8 +17,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -47,6 +46,54 @@ class AuthDocTest {
                         requestFields(
                                 fieldWithPath("email").description("이메일"),
                                 fieldWithPath("password").description("비밀번호")
+                        ),
+
+                        responseFields(
+                                fieldWithPath("accessToken").description("발급된 엑세스토큰"),
+                                fieldWithPath("refreshToken").description("발급된 리프레쉬토큰")
+                        )
+                ));
+    }
+
+    @Test
+    void logout() throws Exception {
+        final AuthToken request = new AuthToken("accessToken", "refreshToken");
+        final String response = AuthService.LOGOUT_SUCCESS;
+        when(authService.logout(any())).thenReturn(response);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        this.mockMvc.perform(post("/authentication/logout")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("auth-logout",
+                        requestFields(
+                                fieldWithPath("accessToken").description("엑세스토큰"),
+                                fieldWithPath("refreshToken").description("리프레쉬토큰")
+                        )
+                ));
+    }
+
+    @Test
+    void reissue() throws Exception {
+        final AuthToken request = new AuthToken("expiredAccessToken", "refreshToken");
+        final AuthToken response = new AuthToken("newAccessToken", "refreshToken");
+        when(authService.reissue(any())).thenReturn(response);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        this.mockMvc.perform(post("/authentication/reissue")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("auth-reissue",
+                        requestFields(
+                                fieldWithPath("accessToken").description("만료된 엑세스토큰"),
+                                fieldWithPath("refreshToken").description("재발급을 위한 리프레쉬토큰")
+                        ),
+
+                        responseFields(
+                                fieldWithPath("accessToken").description("새로 발급된 엑세스토큰"),
+                                fieldWithPath("refreshToken").description("기존의 리프레쉬토큰")
                         )
                 ));
     }

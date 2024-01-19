80ebe3db9f1663fdc336d79433fee814b75f4aae
김명준 <86913355+mjj111@users.noreply.github.com>
2024-01-19 22:30:35 +0900

    feat(MemberError) : 비밀번호 틀림, 엔티티 조회 실패 에러 추가

--- src/main/java/com/aliens/backend/global/error/MemberError.java
+++ src/main/java/com/aliens/backend/global/error/MemberError.java
@@ -4,7 +4,9 @@ public enum MemberError implements ErrorCode{
 
     INVALID_PASSWORD("M1", "올바르지 않은 형식의 비밀번호"),
     INVALID_EMAIL("M2", "올바르지 않은 형식의 이메일"),
-    NULL_MEMBER("M3", "해당 Member 엔티티 조회 불가");
+    NULL_MEMBER("M3", "해당 Member 엔티티 조회 불가"),
+    INCORRECT_PASSWORD("M4", "비밀번호 틀림"),
+    ;
 
     private final String code;
     private final String message;

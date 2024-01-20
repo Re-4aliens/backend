1f0fe1a650752a54c28e47b1ec86bf90981c67f4
김명준 <86913355+mjj111@users.noreply.github.com>
2024-01-20 15:04:42 +0900

    feat(ApiExceptionHandler, GlobalExceptionHandler) : Spring에서 예외를 받아 ResponseEntity로 반환해주는 핸들러 추가

---
+++ src/main/java/com/aliens/backend/global/config/ApiExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.aliens.backend.global.config;
+
+import com.aliens.backend.global.error.ErrorCode;
+import com.aliens.backend.global.exception.RestApiException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Order(value = Integer.MIN_VALUE)
+public class ApiExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger("RestApi 에러 로그");
+
+    @ExceptionHandler(value = RestApiException.class)
+    public ResponseEntity<Object> apiException(RestApiException apiException) {
+        ErrorCode errorCode = apiException.getErrorCode();
+        logger.info(errorCode.getMessage());
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(
+                        errorCode.getDevelopCode()
+                );
+    }
+}

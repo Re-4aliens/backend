f1dd9a0f03cc6af6abfc6e4f329766d43653681d
김명준 <86913355+mjj111@users.noreply.github.com>
2024-01-21 22:51:03 +0900

    test(SymmetricKeyEncoderTest) : 문자열 암호화, 복호화 메서드 테스트 코드 작성

---
+++ src/test/java/com/aliens/backend/encode/SymmetricKeyEncoderTest.java
@@ -0,0 +1,41 @@
+package com.aliens.backend.encode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymmetricKeyEncoderTest {
+
+    @Autowired
+    private SymmetricKeyEncoder symmetricKeyEncoder;
+
+    @Test
+    @DisplayName("문자열 암호화 성공")
+    void encryptTest() throws Exception {
+        //Given
+        String givenInput = "testText";
+
+        //When
+        String result = symmetricKeyEncoder.encrypt(givenInput);
+
+        //Then
+        Assertions.assertNotEquals(result, givenInput);
+    }
+
+    @Test
+    @DisplayName("문자열 복호화 성공")
+    void decryptTest() throws Exception {
+        //Given
+        String givenInput = "testText";
+        String code = symmetricKeyEncoder.encrypt(givenInput);
+
+        //When
+        String result = symmetricKeyEncoder.decrypt(code);
+
+        //Then
+        Assertions.assertEquals(result, givenInput);
+    }
+}

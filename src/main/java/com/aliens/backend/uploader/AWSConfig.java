1f7c7a0929bffa4ce1a9d86e85b0ca1b8f1d32ea
김명준 <86913355+mjj111@users.noreply.github.com>
2024-01-21 22:21:40 +0900

    feat(AWSConfig) : AmazonS3Client 를 얻기위한 설정 클래스 추가

---
+++ src/main/java/com/aliens/backend/uploader/AWSConfig.java
@@ -0,0 +1,27 @@
+package com.aliens.backend.uploader;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSConfig {
+
+    private final S3UploadProperties s3UploadProperties;
+
+    public AWSConfig(final S3UploadProperties s3UploadProperties) {
+        this.s3UploadProperties = s3UploadProperties;
+    }
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(s3UploadProperties.getAccessKey(), s3UploadProperties.getSecretKey());
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(s3UploadProperties.getRegion())
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .build();
+    }
+}

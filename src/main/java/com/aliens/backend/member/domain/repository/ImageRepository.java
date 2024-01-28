61ac9ea8397a1101346b51613323e6cda182a895
김명준 <86913355+mjj111@users.noreply.github.com>
2024-01-28 17:23:59 +0900

    feat(ImageRepository): Image Entity 접근을 위한 레파지토리 추가

---
+++ src/main/java/com/aliens/backend/member/domain/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.aliens.backend.member.domain.repository;
+
+import com.aliens.backend.member.domain.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}

7c3a907573cac647acdba60acf884a4668ea9285
김명준 <86913355+mjj111@users.noreply.github.com>
2024-01-28 16:05:38 +0900

    feat(SymmetricEncoderProperties): chipper 추가 및 예외처리

--- src/main/java/com/aliens/backend/global/encode/SymmetricKeyEncoder.java
+++ src/main/java/com/aliens/backend/global/encode/SymmetricKeyEncoder.java
@@ -1,5 +1,7 @@
 package com.aliens.backend.global.encode;
 
+import com.aliens.backend.global.error.CommonError;
+import com.aliens.backend.global.exception.RestApiException;
 import com.aliens.backend.global.property.SymmetricEncoderProperties;
 import org.springframework.stereotype.Component;
 
@@ -16,18 +18,26 @@ public class SymmetricKeyEncoder {
         this.symmetricEncoderProperties = symmetricEncoderProperties;
     }
 
-    public String encrypt(String plaintext) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, symmetricEncoderProperties.getEncodeKey());
-        byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes(StandardCharsets.UTF_8));
-        return Base64.getEncoder().encodeToString(encryptedBytes);
+    public String encrypt(String plaintext) {
+        try {
+            Cipher cipher = Cipher.getInstance(symmetricEncoderProperties.getChipper());
+            cipher.init(Cipher.ENCRYPT_MODE, symmetricEncoderProperties.getEncodeKey());
+            byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            throw new RestApiException(CommonError.ENCODE_ERROR);
+        }
     }
 
-    public String decrypt(String ciphertext) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, symmetricEncoderProperties.getEncodeKey());
-        byte[] decodedBytes = Base64.getDecoder().decode(ciphertext);
-        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
-        return new String(decryptedBytes, StandardCharsets.UTF_8);
+    public String decrypt(String ciphertext) {
+        try {
+            Cipher cipher = Cipher.getInstance(symmetricEncoderProperties.getChipper());
+            cipher.init(Cipher.DECRYPT_MODE, symmetricEncoderProperties.getEncodeKey());
+            byte[] decodedBytes = Base64.getDecoder().decode(ciphertext);
+            byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+            return new String(decryptedBytes, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new RestApiException(CommonError.ENCODE_ERROR);
+        }
     }
 }

b15a0c1c56718a11298347591dba6f1303f9ae13
Oniqued <zktl3575@gmail.com>
2024-01-30 13:26:55 +0900

    feat(SuccessCode) : 성공 코드, 메시지를 담을 수 있도록 커스텀 ResponseEntity 정의

---
+++ src/main/java/com/aliens/backend/global/success/SuccessResponse.java
@@ -0,0 +1,21 @@
+package com.aliens.backend.global.success;
+
+import org.springframework.http.ResponseEntity;
+
+public class SuccessResponse<T> {
+
+    private String code;
+    private String message;
+    private T result;
+
+    public SuccessResponse(final String code, final String message, final T result) {
+        this.code = code;
+        this.message = message;
+        this.result = result;
+    }
+
+    public static ResponseEntity<SuccessResponse> toResponseEntity(SuccessCode successCode, Object result) {
+        SuccessResponse res = new SuccessResponse(successCode.getCode(), successCode.getMessage(), result);
+        return new ResponseEntity<>(res, successCode.getHttpStatus());
+    }
+}

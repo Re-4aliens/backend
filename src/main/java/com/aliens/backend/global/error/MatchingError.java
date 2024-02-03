9670141950e94953bd2811327973e88f634857c0
Oniqued <zktl3575@gmail.com>
2024-02-03 20:44:13 +0900

    feat : 매칭 신청 입력값 검증 로직 추가

--- src/main/java/com/aliens/backend/global/error/MatchingError.java
+++ src/main/java/com/aliens/backend/global/error/MatchingError.java
@@ -6,7 +6,8 @@ public enum MatchingError implements ErrorCode {
     NOT_FOUND_MATCHING_ROUND(HttpStatus.NOT_FOUND, "MA1", "매칭 회차를 찾을 수 없음"),
     NOT_VALID_MATCHING_RECEPTION_TIME(HttpStatus.BAD_REQUEST, "MA2", "매칭 접수 시간이 아님"),
     NOT_FOUND_MATCHING_APPLICATION_INFO(HttpStatus.NOT_FOUND, "MA3", "매칭 신청 정보 찾을 수 없음"),
-
+    NOT_FOUND_PREFER_LANGUAGE(HttpStatus.NOT_FOUND, "MA4", "선호 언어를 찾을 수 없음"),
+    INVALID_LANGUAGE_INPUT(HttpStatus.BAD_REQUEST, "MA5", "두 선호 언어가 같을 수 없음"),
 
     ;
 
--- src/main/java/com/aliens/backend/mathcing/controller/dto/input/MatchingInput.java
+++ src/main/java/com/aliens/backend/mathcing/controller/dto/input/MatchingInput.java
@@ -1,5 +1,7 @@
 package com.aliens.backend.mathcing.controller.dto.input;
 
+import com.aliens.backend.global.error.MatchingError;
+import com.aliens.backend.global.exception.RestApiException;
 import com.aliens.backend.mathcing.service.model.Language;
 
 import static com.aliens.backend.mathcing.controller.dto.request.MatchingRequest.*;
@@ -9,7 +11,14 @@ public class MatchingInput {
             Language firstPreferLanguage,
             Language secondPreferLanguage) {
         public MatchingApplicationRequest toRequest(final Long memberId){
+            validateInput();
             return new MatchingApplicationRequest(memberId, firstPreferLanguage, secondPreferLanguage);
+        }
+
+        private void validateInput() {
+            if (firstPreferLanguage.equals(secondPreferLanguage)) {
+                throw new RestApiException(MatchingError.INVALID_LANGUAGE_INPUT);
+            }
         }
     }
 }

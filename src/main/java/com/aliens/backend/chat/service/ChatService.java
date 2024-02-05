cd3ac32748e0de2b5236c3438069b0a31ef25c9d
coke98 <jhs9832@gmail.com>
2024-02-06 02:59:31 +0900

    feat(ChatService): 메시지 전송, 메시지 읽음 처리 구현

--- src/main/java/com/aliens/backend/chat/service/ChatService.java
+++ src/main/java/com/aliens/backend/chat/service/ChatService.java
@@ -1,15 +1,72 @@
 package com.aliens.backend.chat.service;
 
+import com.aliens.backend.chat.controller.dto.request.ReadRequest;
+import com.aliens.backend.chat.controller.dto.response.ChatSummary;
+import com.aliens.backend.chat.controller.dto.request.MessageSendRequest;
+import com.aliens.backend.chat.controller.dto.response.ReadResponse;
+import com.aliens.backend.chat.domain.ChatRepository.MessageRepository;
+import com.aliens.backend.chat.domain.Message;
+import com.aliens.backend.global.property.WebSocketProperties;
+import com.aliens.backend.global.success.ChatSuccessCode;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ChatService {
-    public void sendMessage() {
+    private final MessageRepository messageRepository;
+    private final SimpMessagingTemplate messagingTemplate;
+    private final WebSocketProperties webSocketProperties;
+
+    public ChatService(MessageRepository messageRepository, SimpMessagingTemplate messagingTemplate, WebSocketProperties webSocketProperties) {
+        this.messageRepository = messageRepository;
+        this.messagingTemplate = messagingTemplate;
+        this.webSocketProperties = webSocketProperties;
     }
 
-    public void readMessage() {
+    public String sendMessage(MessageSendRequest messageSendRequest) {
+        Message message = Message.of(messageSendRequest);
+        saveMessage(message);
+        publishMessage(message,messageSendRequest.roomId());
+        sendNotification(message);
+        return ChatSuccessCode.SEND_MESSAGE_SUCCESS.getMessage();
     }
 
-    public void getChatSummary() {
+    public String readMessages(ReadRequest readRequest) {
+        updateReadState(readRequest.roomId(), readRequest.memberId());
+        publishReadState(readRequest.roomId(), readRequest.memberId());
+        return ChatSuccessCode.READ_MESSAGES_SUCCESS.getMessage();
+    }
+
+    public List<ChatSummary> getChatSummary() {
+        List<ChatSummary> chatSummarys = new ArrayList<>();
+        return chatSummarys;
+    }
+
+    public List<Message> getMessages() {
+        List<Message> messages = new ArrayList<>();
+        return messages;
+    }
+
+    private void saveMessage(Message message) {
+        messageRepository.save(message);
+    }
+
+    private void publishMessage(Message message, Long ChatRoomId) {
+        messagingTemplate.convertAndSend(webSocketProperties.getTopic()+"/"+ChatRoomId, message);
+    }
+
+    private void sendNotification(Message message) {
+    }
+
+    private void updateReadState(Long chatRoomId, Long readBy) {
+        messageRepository.markMessagesAsRead(chatRoomId, readBy);
+    }
+
+    private void publishReadState(Long chatRoomId, Long memberId) {
+        ReadResponse readResponse = new ReadResponse(memberId);
+        messagingTemplate.convertAndSend(webSocketProperties.getTopic()+"/"+chatRoomId, readResponse);
     }
 }

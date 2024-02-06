ed11b91a769e5f3515a6e4b10b6dda359583cb85
coke98 <jhs9832@gmail.com>
2024-02-07 05:27:02 +0900

    feat(ChatChannelInterceptor): 채팅방 구독시 인가 검증 구현

--- src/main/java/com/aliens/backend/global/interceptor/ChatChannelInterceptor.java
+++ src/main/java/com/aliens/backend/global/interceptor/ChatChannelInterceptor.java
@@ -10,6 +10,8 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class ChatChannelInterceptor implements ChannelInterceptor {
 
@@ -22,8 +24,9 @@ public class ChatChannelInterceptor implements ChannelInterceptor {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-        ChatRoom chatRoom = (ChatRoom) accessor.getSessionAttributes().get("chatRooms");
+        List<ChatRoom> chatRooms = (List<ChatRoom>) accessor.getSessionAttributes().get("chatRooms");
         if (accessor != null && StompCommand.SUBSCRIBE.equals(accessor.getCommand())) {
+            chatAuthValidator.validateRoom(accessor.getDestination(), chatRooms);
         }
         return message;
     }

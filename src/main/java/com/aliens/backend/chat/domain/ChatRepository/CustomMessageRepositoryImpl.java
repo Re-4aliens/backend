64730b60f5670a35f0f602c8a8c8868ba66e2e55
coke98 <jhs9832@gmail.com>
2024-02-07 02:56:56 +0900

    feat(CustomMessageRepository): aggregation을 사용하여 한번의 쿼리로 채팅 메시지 요약 정보를 조회하도록 메소드 구현

--- src/main/java/com/aliens/backend/chat/domain/ChatRepository/CustomMessageRepositoryImpl.java
+++ src/main/java/com/aliens/backend/chat/domain/ChatRepository/CustomMessageRepositoryImpl.java
@@ -1,16 +1,23 @@
 package com.aliens.backend.chat.domain.ChatRepository;
 
 import com.aliens.backend.chat.domain.Message;
+import com.aliens.backend.chat.service.model.ChatMessageSummary;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.ConditionalOperators;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class CustomMessageRepositoryImpl implements CustomMessageRepository {
@@ -42,5 +49,16 @@ public class CustomMessageRepositoryImpl implements CustomMessageRepository {
         }
 
         return mongoTemplate.find(query, Message.class);
+    }
+
+    @Override
+    public List<ChatMessageSummary> aggregateMessageSummaries(List<Long> chatRoomIds, Long memberId) {
+        AggregationOperation match = Aggregation.match(Criteria.where("roomId").in(chatRoomIds));
+        AggregationOperation group = Aggregation.group("roomId")
+                .last("content").as("lastMessageContent")
+                .sum(ConditionalOperators.when(Criteria.where("receiverId").is(memberId).and("isRead").is(false)).then(1).otherwise(0)).as("unreadCount");
+        Aggregation aggregation = Aggregation.newAggregation(match, group);
+        AggregationResults<ChatMessageSummary> results = mongoTemplate.aggregate(aggregation, "message", ChatMessageSummary.class);
+        return new ArrayList<>(results.getMappedResults());
     }
 }

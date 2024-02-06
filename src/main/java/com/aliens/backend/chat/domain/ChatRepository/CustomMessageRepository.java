d134eb9298de40e4a0f7f8e043bad5a45138babc
coke98 <jhs9832@gmail.com>
2024-02-06 23:45:23 +0900

    feat(CustomMessageRepository): 메시지 조회 메서드 구현

--- src/main/java/com/aliens/backend/chat/domain/ChatRepository/CustomMessageRepository.java
+++ src/main/java/com/aliens/backend/chat/domain/ChatRepository/CustomMessageRepository.java
@@ -1,5 +1,10 @@
 package com.aliens.backend.chat.domain.ChatRepository;
 
+import com.aliens.backend.chat.domain.Message;
+
+import java.util.List;
+
 public interface CustomMessageRepository {
     void markMessagesAsRead(Long chatRoomId, Long readBy);
+    List<Message> findMessages(Long chatRoomId, String lastMessageId);
 }
--- src/main/java/com/aliens/backend/chat/domain/ChatRepository/CustomMessageRepositoryImpl.java
+++ src/main/java/com/aliens/backend/chat/domain/ChatRepository/CustomMessageRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.aliens.backend.chat.domain.ChatRepository;
 
 import com.aliens.backend.chat.domain.Message;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class CustomMessageRepositoryImpl implements CustomMessageRepository {
@@ -25,5 +29,18 @@ public class CustomMessageRepositoryImpl implements CustomMessageRepository {
         Update update = new Update();
         update.set("isRead", true);
         mongoTemplate.updateMulti(query, update, Message.class);
+    }
+
+    @Override
+    public List<Message> findMessages(Long chatRoomID, String lastMessageId) {
+        Query query = new Query(
+                Criteria.where("roomId").is(chatRoomID)
+        ).with(Sort.by(Sort.Direction.DESC, "_id")).limit(100); // "_id"를 사용하여 정렬
+
+        if (lastMessageId != null) {
+            query.addCriteria(Criteria.where("_id").lt(new ObjectId(lastMessageId)));
+        }
+
+        return mongoTemplate.find(query, Message.class);
     }
 }

ba33ab0279e780b6757ef3ccb492f4fe70b89559
coke98 <jhs9832@gmail.com>
2024-02-07 02:45:47 +0900

    feat(ChatSummaryResponse): ChatSummary 삭제 및 ChatSummaryResponse 추가

--- src/main/java/com/aliens/backend/chat/controller/dto/response/ChatSummary.java
+++
@@ -1,6 +0,0 @@
-package com.aliens.backend.chat.controller.dto.response;
-
-import com.aliens.backend.chat.domain.Message;
-
-public record ChatSummary(Long roomId, Long partnerId, Message lastMessage, Long numberOfUnreadMessages) {
-}

ae98257d28eb54538f4e60dcfcdea68bdf6809fc
coke98 <jhs9832@gmail.com>
2024-02-07 06:34:19 +0900

    feat(ChatReportController): 채팅 신고 컨트롤러 추가

---
+++ src/main/java/com/aliens/backend/chat/controller/ChatReportController.java
@@ -0,0 +1,30 @@
+package com.aliens.backend.chat.controller;
+
+import com.aliens.backend.chat.controller.dto.request.ChatReportRequest;
+import com.aliens.backend.chat.service.ChatReportService;
+import com.aliens.backend.global.config.resolver.Login;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Collections;
+import java.util.Map;
+
+
+@Controller
+public class ChatReportController {
+
+    private final ChatReportService chatReportService;
+
+    public ChatReportController(ChatReportService chatReportService) {
+        this.chatReportService = chatReportService;
+    }
+
+    @PostMapping("/chat/report")
+    public ResponseEntity<Map<String, String>> reportPartner(@Login Long memberId, @RequestBody ChatReportRequest chatReportRequest) {
+        String result = chatReportService.reportPartner(memberId, chatReportRequest);
+        Map<String, String> response = Collections.singletonMap("response", result);
+        return ResponseEntity.ok(response);
+    }
+}

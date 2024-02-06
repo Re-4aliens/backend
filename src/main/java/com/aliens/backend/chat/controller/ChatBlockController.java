ecb842ad12e060cb76c57461c548387223bce8ea
coke98 <jhs9832@gmail.com>
2024-02-07 07:00:00 +0900

    feat(ChatBlockController, ChatBlockRequest): 채팅 차단 컨트롤러 추가, 차단 요청 레코드 추가

---
+++ src/main/java/com/aliens/backend/chat/controller/ChatBlockController.java
@@ -0,0 +1,30 @@
+package com.aliens.backend.chat.controller;
+
+import com.aliens.backend.chat.controller.dto.request.ChatBlockRequest;
+import com.aliens.backend.chat.service.ChatBlockService;
+import com.aliens.backend.global.config.resolver.Login;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Collections;
+import java.util.Map;
+
+
+@Controller
+public class ChatBlockController {
+
+    private final ChatBlockService chatBlockService;
+
+    public ChatBlockController(ChatBlockService chatBlockService) {
+        this.chatBlockService = chatBlockService;
+    }
+
+    @PostMapping("/chat/block")
+    public ResponseEntity<Map<String, String>> blockPartner(@Login Long memberId, @RequestBody ChatBlockRequest chatBlockRequest) {
+        String result = chatBlockService.blockPartner(memberId, chatBlockRequest);
+        Map<String, String> response = Collections.singletonMap("response", result);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/aliens/backend/chat/controller/dto/request/ChatBlockRequest.java
@@ -0,0 +1,4 @@
+package com.aliens.backend.chat.controller.dto.request;
+
+public record ChatBlockRequest(Long partnerId, Long chatRoomId) {
+}

fe5bd2ac2955c2e252efc24978c11ab3c76b8a85
coke98 <jhs9832@gmail.com>
2024-02-07 02:50:24 +0900

    feat(ChatController): 메시지 요약 정보 조회 메서드 구현

--- src/main/java/com/aliens/backend/chat/controller/ChatController.java
+++ src/main/java/com/aliens/backend/chat/controller/ChatController.java
@@ -2,10 +2,21 @@ package com.aliens.backend.chat.controller;
 
 import com.aliens.backend.chat.controller.dto.request.MessageSendRequest;
 import com.aliens.backend.chat.controller.dto.request.ReadRequest;
+import com.aliens.backend.chat.controller.dto.response.ChatSummaryResponse;
+import com.aliens.backend.chat.domain.Message;
 import com.aliens.backend.chat.service.ChatService;
+import com.aliens.backend.global.config.resolver.Login;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 
 @Controller
@@ -27,14 +38,16 @@ public class ChatController {
         chatService.readMessages(readRequest);
     }
 
-    @MessageMapping("/summary")
-    public void getChatSummary() {
-        chatService.getChatSummary();
+    @GetMapping("chat/summaries")
+    public ResponseEntity<Map<String, ChatSummaryResponse>> getChatSummaries(@Login Long memberId) {
+        ChatSummaryResponse chatSummaryResponse = chatService.getChatSummaries(memberId);
+        Map<String, ChatSummaryResponse> response = Collections.singletonMap("response", chatSummaryResponse);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/chat/room/{roomId}/messages")
     public ResponseEntity<Map<String, List>> getMessages(@PathVariable("roomId") Long chatRoomId,
-                                                         @RequestParam("lastMessageId") String lastMessageId) {
+                                                         @RequestParam(value = "lastMessageId", required = false) String lastMessageId) {
         List<Message> messages = chatService.getMessages(chatRoomId,lastMessageId);
         Map<String, List> response = Collections.singletonMap("response", messages);
         return ResponseEntity.ok(response);

cacd90bfef49e41e0bdfbc80e5b27cd3b03ee626
김명준 <86913355+mjj111@users.noreply.github.com>
2024-02-12 19:46:55 +0900

    refactor(ChatReportService) : 멤버 객체와 연관관계 추가로 인한 코드 수정
    #47

--- src/main/java/com/aliens/backend/chat/service/ChatReportService.java
+++ src/main/java/com/aliens/backend/chat/service/ChatReportService.java
@@ -1,23 +1,47 @@
 package com.aliens.backend.chat.service;
 
+import com.aliens.backend.auth.controller.dto.LoginMember;
+import com.aliens.backend.auth.domain.Member;
+import com.aliens.backend.auth.domain.repository.MemberRepository;
 import com.aliens.backend.chat.controller.dto.request.ChatReportRequest;
 import com.aliens.backend.chat.domain.ChatReport;
 import com.aliens.backend.chat.domain.repository.ChatReportRepository;
-import com.aliens.backend.global.success.ChatSuccessCode;
+import com.aliens.backend.global.exception.RestApiException;
+import com.aliens.backend.global.response.error.MemberError;
+import com.aliens.backend.global.response.success.ChatSuccess;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatReportService {
 
     private final ChatReportRepository chatReportRepository;
+    private final MemberRepository memberRepository;
 
-    public ChatReportService(ChatReportRepository chatReportRepository) {
+    public ChatReportService(ChatReportRepository chatReportRepository, final MemberRepository memberRepository) {
         this.chatReportRepository = chatReportRepository;
+        this.memberRepository = memberRepository;
     }
 
-    public String reportPartner(Long memberId, ChatReportRequest chatReportRequest) {
-        ChatReport chatReport = ChatReport.of(memberId, chatReportRequest);
+    public String reportPartner(LoginMember loginMember, ChatReportRequest chatReportRequest) {
+        Member member = getMember(loginMember);
+        Member partner = findMemberById(chatReportRequest.partnerId());
+
+        ChatReport chatReport = ChatReport.of(
+                member,
+                partner,
+                chatReportRequest.category(),
+                chatReportRequest.content());
+
         chatReportRepository.save(chatReport);
-        return ChatSuccessCode.REPORT_SUCCESS.getMessage();
+
+        return ChatSuccess.REPORT_SUCCESS.getMessage();
+    }
+
+    private Member findMemberById(final Long partnerId) {
+        return memberRepository.findById(partnerId).orElseThrow(() -> new RestApiException(MemberError.NULL_MEMBER));
+    }
+
+    private Member getMember(final LoginMember loginMember) {
+        return memberRepository.findById(loginMember.memberId()).orElseThrow(() -> new RestApiException(MemberError.NULL_MEMBER));
     }
 }

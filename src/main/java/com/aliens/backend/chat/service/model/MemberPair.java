6fdcc9ab435f95fd2587a286e6d7045d7c126dbb
coke98 <jhs9832@gmail.com>
2024-02-15 21:40:05 +0900

    feat(MemberPair): MemberPair 클래스 추가

---
+++ src/main/java/com/aliens/backend/chat/service/model/MemberPair.java
@@ -0,0 +1,25 @@
+package com.aliens.backend.chat.service.model;
+
+import com.aliens.backend.auth.domain.Member;
+
+import java.util.Objects;
+
+public record MemberPair(Member first, Member second) {
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        MemberPair that = (MemberPair) object;
+        return (Objects.equals(first, that.first) && Objects.equals(second, that.second)) ||
+                (Objects.equals(first, that.second) && Objects.equals(second, that.first));
+    }
+
+    @Override
+    public int hashCode() {
+        if (first.getId() < second.getId()) {
+            return Objects.hash(first, second);
+        } else {
+            return Objects.hash(second, first);
+        }
+    }
+}

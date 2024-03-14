a67c72613328af9b1744c1813da260026c957620
Oniqued <zktl3575@gmail.com>
2024-03-14 11:29:53 +0900

    fix(MatchingRestDocsTest) : RestDoc 테스트 병합 충돌 해결

--- src/test/java/com/aliens/backend/docs/MatchingRestDocsTest.java
+++ src/test/java/com/aliens/backend/docs/MatchingRestDocsTest.java
@@ -2,7 +2,6 @@ package com.aliens.backend.docs;
 
 import com.aliens.backend.auth.controller.dto.LoginMember;
 import com.aliens.backend.auth.domain.Member;
-import com.aliens.backend.global.BaseServiceTest;
 import com.aliens.backend.global.DummyGenerator;
 import com.aliens.backend.matching.util.time.MockClock;
 import com.aliens.backend.matching.util.time.MockTime;
@@ -16,9 +15,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,10 +26,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.response
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@SpringBootTest
-class MatchingDocTest extends BaseServiceTest {
+class MatchingRestDocsTest extends BaseRestDocsTest {
     @Autowired MockClock mockClock;
     @Autowired MockMvc mockMvc;
     @Autowired MatchingApplicationController matchingApplicationController;

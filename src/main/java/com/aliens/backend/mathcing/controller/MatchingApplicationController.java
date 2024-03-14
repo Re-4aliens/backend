d11a40965fcdb8b83e2f42a47488812212c02eb5
Oniqued <zktl3575@gmail.com>
2024-03-14 13:22:15 +0900

    feat : 매칭 시작 시간 조회 기능 추가

--- src/main/java/com/aliens/backend/mathcing/controller/MatchingApplicationController.java
+++ src/main/java/com/aliens/backend/mathcing/controller/MatchingApplicationController.java
@@ -7,11 +7,12 @@ import com.aliens.backend.global.response.success.MatchingSuccess;
 import com.aliens.backend.global.validator.LanguageCheck;
 import com.aliens.backend.mathcing.controller.dto.request.MatchingApplicationRequest;
 import com.aliens.backend.mathcing.controller.dto.response.MatchingApplicationResponse;
+import com.aliens.backend.mathcing.controller.dto.response.MatchingBeginTimeResponse;
 import com.aliens.backend.mathcing.service.MatchingApplicationService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/matchings")
+@RequestMapping("/matchings/applications")
 public class MatchingApplicationController {
     private final MatchingApplicationService matchingApplicationService;
 
@@ -19,20 +20,26 @@ public class MatchingApplicationController {
         this.matchingApplicationService = matchingApplicationService;
     }
 
-    @PostMapping("/applications")
+    @PostMapping
     public SuccessResponse<String> applyMatch(final @Login LoginMember loginMember,
                                               final @RequestBody @LanguageCheck MatchingApplicationRequest matchingApplicationRequest) {
         return SuccessResponse.of(MatchingSuccess.APPLY_MATCHING_SUCCESS,
                 matchingApplicationService.saveParticipant(loginMember, matchingApplicationRequest));
     }
 
-    @GetMapping("/applications")
+    @GetMapping
     public SuccessResponse<MatchingApplicationResponse> getMatchingApplication(final @Login LoginMember loginMember) {
         return SuccessResponse.of(MatchingSuccess.GET_MATCHING_APPLICATION_STATUS_SUCCESS,
                 matchingApplicationService.findMatchingApplication(loginMember));
     }
 
-    @DeleteMapping("/applications")
+    @GetMapping("/begin-time")
+    public SuccessResponse<MatchingBeginTimeResponse> getMatchingBeginTime() {
+        return SuccessResponse.of(MatchingSuccess.GET_MATCHING_BEGIN_TIME_SUCCESS,
+                matchingApplicationService.findMatchingBeginTime());
+    }
+
+    @DeleteMapping
     public SuccessResponse<String> cancelMatchingApplication(final @Login LoginMember loginMember) {
         return SuccessResponse.of(MatchingSuccess.CANCEL_MATCHING_APPLICATION_SUCCESS,
                 matchingApplicationService.cancelMatchingApplication(loginMember));
---
+++ src/main/java/com/aliens/backend/mathcing/controller/dto/response/MatchingBeginTimeResponse.java
@@ -0,0 +1,14 @@
+package com.aliens.backend.mathcing.controller.dto.response;
+
+import com.aliens.backend.mathcing.domain.MatchingRound;
+
+import java.time.LocalDateTime;
+
+public record MatchingBeginTimeResponse(
+        Long round,
+        LocalDateTime matchingBeginTime
+) {
+    public static MatchingBeginTimeResponse from(MatchingRound matchingRound) {
+        return new MatchingBeginTimeResponse(matchingRound.getRound(), matchingRound.getValidStartTime());
+    }
+}

d31e75379237f0fea65066ae323bce3d249a76a3
김명준 <86913355+mjj111@users.noreply.github.com>
2024-04-25 16:36:13 +0900

    feat(BoardController) : 게시판 컨트롤러 추가

---
+++ src/main/java/com/aliens/backend/board/controller/BoardController.java
@@ -0,0 +1,87 @@
+package com.aliens.backend.board.controller;
+
+
+import com.aliens.backend.auth.controller.dto.LoginMember;
+import com.aliens.backend.board.controller.dto.request.BoardCreateRequest;
+import com.aliens.backend.board.controller.dto.response.BoardResponse;
+import com.aliens.backend.board.service.BoardReadService;
+import com.aliens.backend.board.service.BoardService;
+import com.aliens.backend.global.config.resolver.Login;
+import com.aliens.backend.global.response.SuccessResponse;
+import com.aliens.backend.global.response.success.BoardSuccess;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RequestMapping("/boards")
+@RestController
+public class BoardController {
+
+    private final BoardService boardService;
+    private final BoardReadService boardReadService;
+
+    public BoardController(final BoardService boardService, final BoardReadService boardReadService) {
+        this.boardService = boardService;
+        this.boardReadService = boardReadService;
+    }
+
+    @PostMapping("/normal")
+    public SuccessResponse<?> createBoard(@Login final LoginMember loginMember,
+                                       @RequestBody final BoardCreateRequest request,
+                                       @RequestPart final List<MultipartFile> boardImages) {
+        boardService.postNormalBoard(request, boardImages, loginMember);
+        return SuccessResponse.of(BoardSuccess.POST_BOARD_SUCCESS);
+    }
+
+    @GetMapping
+    public SuccessResponse<List<BoardResponse>> getAllBoards(@PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        return SuccessResponse.of(BoardSuccess.GET_ALL_BOARDS_SUCCESS,
+                boardReadService.getBoardPage(pageable));
+    }
+
+    @GetMapping("/category")
+    public SuccessResponse<List<BoardResponse>> getAllBoardsWithCategory(@RequestParam final String category,
+                                                                         @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) final Pageable pageable) {
+        return SuccessResponse.of(BoardSuccess.GET_ALL_BOARDS_WITH_CATEGORY_SUCCESS,
+                boardReadService.getBoardPageWithCategory(category,pageable));
+    }
+
+    @GetMapping("/search")
+    public SuccessResponse<List<BoardResponse>> searchAllBoardPage(@RequestParam("search-keyword") final String searchKeyword,
+                                                                   @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) final Pageable pageable) {
+        return SuccessResponse.of(BoardSuccess.SEARCH_ALL_BOARDS_SUCCESS,
+                boardReadService.searchBoardPageWithKeyword(searchKeyword,pageable));
+    }
+
+    @GetMapping("/announcements")
+    public SuccessResponse<List<BoardResponse>> getAllAnnouncementBoards(@PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) final Pageable pageable) {
+        return SuccessResponse.of(BoardSuccess.GET_ALL_ANNOUNCEMENT_BOARDS_SUCCESS,
+                boardReadService.getAnnouncePage(pageable));
+    }
+
+    @GetMapping("/writes")
+    public SuccessResponse<List<BoardResponse>> getPageMyBoards(@Login LoginMember loginMember,
+                                                                @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) final Pageable pageable) {
+        return SuccessResponse.of(BoardSuccess.GET_MY_BOARD_PAGE_SUCCESS,
+                boardReadService.getMyBoardPage(loginMember, pageable));
+    }
+
+    @GetMapping("/category/search")
+    public SuccessResponse<List<BoardResponse>> searchBoardsWithCategory(@RequestParam("search-keyword") final String searchKeyword,
+                                                                         @RequestParam("category") final String category,
+                                                                         @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) final Pageable pageable) {
+        return SuccessResponse.of(BoardSuccess.SEARCH_BOARD_WITH_CATEGORY_SUCCESS,
+                boardReadService.searchBoardPageWithKeywordAndCategory(searchKeyword, category, pageable));
+    }
+
+    @DeleteMapping
+    public SuccessResponse<?> deleteBoard(@Login final LoginMember loginMember,
+                            @RequestParam("id") final Long id) {
+        boardService.deleteBoard(loginMember, id);
+        return SuccessResponse.of(BoardSuccess.DELETE_BOARD_SUCCESS);
+    }
+}

a92e71d773b3960d95d27ef8d775e10c597b075c
김명준 <86913355+mjj111@users.noreply.github.com>
2024-04-25 16:52:58 +0900

    feat(BoardImage) : 게시판 이미지 엔티티 추가

---
+++ src/main/java/com/aliens/backend/board/domain/BoardImage.java
@@ -0,0 +1,44 @@
+package com.aliens.backend.board.domain;
+
+import com.aliens.backend.uploader.dto.S3File;
+import jakarta.persistence.*;
+
+@Entity
+public class BoardImage {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    String name;
+
+    @Column
+    String url;
+
+    public void setBoard(final Board board) {
+        this.board = board;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    protected BoardImage() {
+    }
+
+    public static BoardImage from(final S3File imageFile) {
+        BoardImage boardImage = new BoardImage();
+        boardImage.name = imageFile.fileName();
+        boardImage.url = imageFile.fileURL();
+        return boardImage;
+    }
+
+    public String getURL() {
+        return url;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

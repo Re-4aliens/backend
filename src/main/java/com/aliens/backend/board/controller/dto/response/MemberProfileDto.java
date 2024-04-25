ff99aaa40267ebf3acc5c2de4670c3c43c24809d
김명준 <86913355+mjj111@users.noreply.github.com>
2024-04-25 17:00:33 +0900

    feat(MemberProfileDto) : 게시판 관련 dto에 들어가는 회원정보 dto 추가

---
+++ src/main/java/com/aliens/backend/board/controller/dto/response/MemberProfileDto.java
@@ -0,0 +1,6 @@
+package com.aliens.backend.board.controller.dto.response;
+
+public record MemberProfileDto( String name,
+                                String profileImageUrl,
+                                String nationality) {
+}

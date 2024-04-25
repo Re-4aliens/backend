c08aea50dd146edff4355d65693ed96e14ed1a69
김명준 <86913355+mjj111@users.noreply.github.com>
2024-04-25 17:08:04 +0900

    feat(BoardReportRepository) : 레파지토리 애너테이션 추가

--- src/main/java/com/aliens/backend/board/domain/repository/BoardReportRepository.java
+++ src/main/java/com/aliens/backend/board/domain/repository/BoardReportRepository.java
@@ -2,7 +2,9 @@ package com.aliens.backend.board.domain.repository;
 
 import com.aliens.backend.board.domain.BoardReport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardReportRepository extends JpaRepository<BoardReport, Long> {
     Long countByBoardId(Long boardId);
 }

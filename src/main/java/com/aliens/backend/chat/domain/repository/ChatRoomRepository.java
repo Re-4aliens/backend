2abae3af0b0a4c2a57927a761921f083234fba79
coke98 <jhs9832@gmail.com>
2024-07-27 16:10:55 +0900

    feat: 채팅방 전체 만료 레포지토리 쿼리 추가
    ref: #136

--- src/main/java/com/aliens/backend/chat/domain/repository/ChatRoomRepository.java
+++ src/main/java/com/aliens/backend/chat/domain/repository/ChatRoomRepository.java
@@ -2,6 +2,7 @@ package com.aliens.backend.chat.domain.repository;
 
 import com.aliens.backend.chat.domain.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,8 @@ import java.util.List;
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     @Query("SELECT c FROM ChatRoom c JOIN c.participants p WHERE p.member.id = :memberId")
     List<ChatRoom> findByMemberId(Long memberId);
+
+    @Modifying
+    @Query("UPDATE ChatRoom c SET c.status = com.aliens.backend.chat.domain.ChatRoomStatus.CLOSED")
+    void expireAllChatRooms();
 }

a43117d23465d679b7e7f089cd18bb5ecdb50747
Oniqued <zktl3575@gmail.com>
2024-09-11 18:50:05 +0900

    feat(MatchingLogAspect) : matching 도메인 AOP 로깅 추가

--- src/main/java/com/aliens/backend/global/response/success/MatchingSuccess.java
+++ src/main/java/com/aliens/backend/global/response/success/MatchingSuccess.java
@@ -9,6 +9,9 @@ public enum MatchingSuccess implements SuccessCode {
     GET_MATCHING_PARTNERS_SUCCESS(HttpStatus.OK, "MA004", "매칭 파트너 조회 성공"),
     GET_MATCHING_BEGIN_TIME_SUCCESS(HttpStatus.OK, "MA005", "매칭 시간 시간 조회 성공"),
     MODIFY_MATCHING_APPLICATION_INFO_SUCCESS(HttpStatus.OK, "MA006", "매칭 신청 정보 수정 성공"),
+    EXPIRE_MATCHING_SUCCESS(HttpStatus.OK, "MA007", "매칭 만료 성공"),
+    OPERATE_MATCHING_SUCCESS(HttpStatus.OK, "MA008", "매칭 시스템 실행 성공"),
+    CREATE_MATCHING_ROUND_SUCCESS(HttpStatus.OK, "MA009", "매칭 회차 생성 성공"),
     ;
 
     private final HttpStatus httpStatus;

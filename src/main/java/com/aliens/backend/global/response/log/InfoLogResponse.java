9192fa289bb967bb73651c78771c22ea40fdc352
Oniqued <zktl3575@gmail.com>
2024-09-11 16:20:49 +0900

    feat(InfoLogResponse) : INFO 레벨 로그의 표현 형태를 정의한 클래스 생성

---
+++ src/main/java/com/aliens/backend/global/response/log/InfoLogResponse.java
@@ -0,0 +1,48 @@
+package com.aliens.backend.global.response.log;
+
+import com.aliens.backend.global.response.error.ErrorCode;
+import com.aliens.backend.global.response.success.SuccessCode;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+public record InfoLogResponse(
+        String logId,
+        String status,
+        String message,
+        Object data
+) {
+    private static final int MAX_STACK_TRACE_ELEMENTS = 5;
+
+    public static InfoLogResponse from(SuccessCode code, Object data) {
+        return new InfoLogResponse(
+                UUID.randomUUID().toString().substring(0, 8),
+                code.getHttpStatus().toString(),
+                code.getMessage(),
+                data);
+    }
+
+    public static InfoLogResponse from(ErrorCode code, Object data) {
+        return new InfoLogResponse(
+                UUID.randomUUID().toString().substring(0, 8),
+                code.getHttpStatus().toString(),
+                code.getMessage(),
+                data);
+    }
+
+    public static InfoLogResponse from(Throwable exception, Object data) {
+        StackTraceElement[] stackTrace = exception.getStackTrace();
+        String limitedStackTrace = Arrays.stream(stackTrace)
+                .limit(MAX_STACK_TRACE_ELEMENTS)
+                .map(StackTraceElement::toString)
+                .reduce((s1, s2) -> s1 + ", " + s2)
+                .orElse("No stack trace available");
+
+        return new InfoLogResponse(
+                UUID.randomUUID().toString().substring(0, 8),
+                exception.getMessage(),
+                limitedStackTrace,
+                data
+        );
+    }
+}

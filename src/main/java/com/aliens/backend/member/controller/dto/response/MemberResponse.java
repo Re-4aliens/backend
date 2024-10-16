cf2658bb1433c43deeae61e7978bad16076621fb
김명준 <86913355+mjj111@users.noreply.github.com>
2024-10-16 19:43:38 +0900

    fix (MemberInfoService) : TCP 재요청으로 인한 중복회원 가입 방지

--- src/main/java/com/aliens/backend/member/controller/dto/response/MemberResponse.java
+++ src/main/java/com/aliens/backend/member/controller/dto/response/MemberResponse.java
@@ -1,7 +1,7 @@
 package com.aliens.backend.member.controller.dto.response;
 
 public enum MemberResponse {
-
+    ALREADY_SIGN_UP("이미 회원 가입되어 있습니다."),
     SIGN_UP_SUCCESS("회원가입이 완료되었습니다."),
     WITHDRAW_SUCCESS("회원 탈퇴되었습니다."),
     TEMPORARY_PASSWORD_GENERATED_SUCCESS("임시 비밀번호가 발급되었습니다. 이메일을 확인해주세요."),

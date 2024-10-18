74c4ea333fac661d18c0f4e852b7783c36d8e6ba
Oniqued <zktl3575@gmail.com>
2024-10-18 13:59:17 +0900

    feat(AsyncConfig) : 비동기 설정이 dev, prod 환경에서만 비동기적으로 동작하도록 설정

--- src/main/java/com/aliens/backend/global/config/AsyncConfig.java
+++ src/main/java/com/aliens/backend/global/config/AsyncConfig.java
@@ -1,16 +1,17 @@
 package com.aliens.backend.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
+@Profile({"dev", "prod"})
 @EnableAsync
 @Configuration
 public class AsyncConfig implements AsyncConfigurer {
-
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
